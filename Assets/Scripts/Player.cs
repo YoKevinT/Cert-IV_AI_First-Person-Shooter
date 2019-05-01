5aa489433225caed705ae5833d83b8939262f514
Kevin Turcumani <kevinjaja98@gmail.com>
2019-05-01 12:05:04 +1000

    Day 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	public float runSpeed = 8f;
-	public float walkSpped = 6f;
-	public float gravity = 15f;
-	public float crouchSpeed = 4f;
-	public float jumpHeight = 8f;
-	public float groundRayDistance = 1.1f;
-	
+    public float runSpeed = 8f;
+    public float walkSpped = 6f;
+    public float gravity = 15f;
+    public float crouchSpeed = 4f;
+    public float jumpHeight = 8f;
+    public float groundRayDistance = 1.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
