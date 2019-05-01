2e9b0f7d0cb41c66cb8ca548337564e86d840a8c
Kevin Turcumani <kevinjaja98@gmail.com>
2019-05-01 10:28:58 +1000

    Basic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public float runSpeed = 8f;
+	public float walkSpped = 6f;
+	public float gravity = 15f;
+	public float crouchSpeed = 4f;
+	public float jumpHeight = 8f;
+	public float groundRayDistance = 1.1f;
+	
     // Start is called before the first frame update
     void Start()
     {
