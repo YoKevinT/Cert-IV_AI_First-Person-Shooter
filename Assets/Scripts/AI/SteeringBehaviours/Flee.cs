64cdc83ce9d608d756f07620346be6a09cd3d782
Kevin Turcumani <kevinjaja98@gmail.com>
2019-06-12 09:19:46 +1000

    Week 7.3
    Flee Added

--- Assets/Scripts/AI/SteeringBehaviours/Flee.cs
+++ Assets/Scripts/AI/SteeringBehaviours/Flee.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flee : MonoBehaviour
+public class Flee : SteeringBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
 
-    // Update is called once per frame
-    void Update()
+    public override Vector3 GetForce(AI owner)
     {
-        
+        // SET force to zero
+        Vector3 force = Vector3.zero;
+
+        // IF target is not null
+        if (target) // shorthand for target != null
+        {
+            // SET desiredForce to target - current (position)
+            Vector3 desiredForce = target.position - transform.position;
+            // SET force to desiredForce normalized x weighting
+            force -= desiredForce.normalized * weighting;
+        }
+
+        // RETURN force
+        return force;
     }
 }
